5cd6021bf20be699caec26a52785100ff083878a
Mahammad Osmanov <54234253+mahammadosmanov@users.noreply.github.com>
2024-02-11 23:42:55 +0400

    auth_manager write

--- lib/app/managers/auth_manager.dart
+++ lib/app/managers/auth_manager.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
 import 'package:flutter/material.dart';
 
 class AuthManager extends ChangeNotifier{
@@ -10,20 +11,41 @@ class AuthManager extends ChangeNotifier{
   
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+  
   String verificationId = "";
+  int forceResendingToken = 0;
+
+  void signInWithPhone(String phoneNumber,String smsCode,) async {
+    ConfirmationResult confirmationResult = await _auth.signInWithPhoneNumber(phoneNumber,RecaptchaVerifier(auth: FirebaseAuthPlatform.instance,container: 'recaptcha',size: RecaptchaVerifierSize.compact,
+  theme: RecaptchaVerifierTheme.dark,onError: (exception) {
+    //TODO error handling
+  },onExpired: () {
+    //TODO on expired
+  },
+  onSuccess: () {
+    //TODO on success
+  },),);
+    UserCredential userCredential = await confirmationResult.confirm(smsCode);
+  }
+
+  
 
   Future<void> verifyPhoneNumber(String phoneNumber) async {
-    _auth.verifyPhoneNumber(verificationCompleted: (phoneAuthCredential) async {
+    _auth.verifyPhoneNumber(
+      
+      verificationCompleted: (phoneAuthCredential) async {
       await _auth.signInWithCredential(phoneAuthCredential);
-    },
+          },
         verificationFailed: (error) {
-
+          //TODO error handling
         },
         codeSent: (verificationId, forceResendingToken) {
           verificationId = verificationId;
-        },
+          forceResendingToken = forceResendingToken;  
+          },
         codeAutoRetrievalTimeout: (verificationId) {
-          //TODO
+          
         },
     );
   }
@@ -31,10 +53,5 @@ class AuthManager extends ChangeNotifier{
   Future<UserCredential> verifySms(String smsCode) async {
     PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsCode);
     return await _auth.signInWithCredential(credential);
-  }
-    
-  @override
-  void dispose() {
-    super.dispose();
   }
 }
